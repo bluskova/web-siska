352e5573c99901ecc316b9018d54677fa3435725
bluskova <barbora.luskova@gmail.com>
2024-10-05 16:48:37 +0200

    wip

--- js/main.js
+++ js/main.js
@@ -28,8 +28,4 @@ document.addEventListener("DOMContentLoaded", () => {
   if (document.getElementById("introduction-text")) {
     loadHtmlFragment("introduction-text.html", "introduction-text");
   }
-
-  if (document.getElementById("harmonogram-table-2024")) {
-    loadHtmlFragment("harmonogram-2024.html", "harmonogram-table-2024");
-  }
 });
